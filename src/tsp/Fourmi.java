40d0ec1d7866d89a878ccc645339bba05aabc102
Pancake39 <emilie.lhomme@laposte.net>
2018-10-24 12:03:50 +0200

    Fouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuurmi

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -4,7 +4,7 @@ public class Fourmi {
 	
 	private Solution Trajet_fourmi; // rempli dans l'ordre des villes par lesquelles est passée la fourmi
 	
-	private ArrayList<Integer> ville_non_visitees;												 
+	private boolean visited;												 
 	private int position;
 	private Instance instance;
 	
@@ -12,6 +12,7 @@ public class Fourmi {
 	public Fourmi(Instance instance) throws Exception{ //constructeur pour les fourmis 
 		this.instance=instance;
 		this.position=0;
+		this.Trajet_fourmi = new Solution(instance);
 		this.Trajet_fourmi.setCityPosition(0,0); // initialisation du trajet à la ville de départ soit la ville 0
 		this.Trajet_fourmi.setCityPosition(0, this.instance.getNbCities()-1);//retour à la ville de départ 
 		for(int i=0;i<this.instance.getNbCities()-2;i++) {//init donc toutes les villes sauf celle de départ
