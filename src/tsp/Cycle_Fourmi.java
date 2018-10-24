29314ea772b410ce3409aafb390d32046306589d
luckythomas <t.oustric@gmail.com>
2018-10-24 11:46:30 +0200

    fourmiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ src/tsp/Cycle_Fourmi.java
@@ -0,0 +1,8 @@
+package tsp;
+
+public class Cycle_Fourmi {
+	
+	private Double[][] Pheromone_trajet_entre_deux_villes; // matrice donnant la quantité de phéromone entre la ville i et j
+														   //implémenté à la fin de chaque cycle
+
+}
--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -2,44 +2,55 @@ package tsp;
 import java.util.ArrayList;
 public class Fourmi {
 	
-	private ArrayList<Integer> Trajet_fourmi; // rempli dans l'ordre des villes par lesquelles est passée la fourmi
-	private Double[][] Proba_trajet_entre_deux_villes; // matrice donnant la proba d'emprunter le chemin entre la ville i et la ville j
-													  // rempli/implémenté à la fin de chaque trajet de chaque fourmi
+	private Solution Trajet_fourmi; // rempli dans l'ordre des villes par lesquelles est passée la fourmi
+	
+	private ArrayList<Integer> ville_non_visitees;												 
 	private int position;
 	private Instance instance;
 	
-	public Fourmi(Instance instance) {
+	
+	public Fourmi(Instance instance) throws Exception{ //constructeur pour les fourmis 
 		this.instance=instance;
 		this.position=0;
-		this.Trajet_fourmi.add(0); // initialisation du trajet à la ville de départ soit la ville 0
+		this.Trajet_fourmi.setCityPosition(0,0); // initialisation du trajet à la ville de départ soit la ville 0
+		this.Trajet_fourmi.setCityPosition(0, this.instance.getNbCities()-1);//retour à la ville de départ 
+		for(int i=0;i<this.instance.getNbCities()-2;i++) {//init donc toutes les villes sauf celle de départ
+			this.ville_non_visitees.add(i+1);
+		}
 		/*for(int i=0;i<this.instance.getNbCities();i++) {
 			this.Trajet_fourmi.add(0);
 			this.Proba_ville.add(0.0);
 		}*/ // initialisation vide utile ?
 		
+		
 	}
 	
-	public void etape_trajet_fourmi(int ville_i) { //chemin entre la dernière ville visitée et la ville suivante (ici la ville i)
-		this.Trajet_fourmi.add(ville_i); // ajoute la ville i au trajet 
+	public void etape_trajet_fourmi(int ville_i, int index_ville_actuelle) throws Exception{ //chemin entre la dernière ville visitée et la ville suivante (ici la ville i)
+		this.Trajet_fourmi.setCityPosition(ville_i, index_ville_actuelle+1); // ajoute la ville i au trajet 
 		this.position = ville_i;  // mise à jour de la position de la fourmi
+		this.ville_non_visitees.remove(ville_i); // enlève la ville_i des villes non visitées
 		
-		// !! vérifier que ville_i non visitée
+		// !! vérifier que ville_i non visitée et que index_ville_actuelle<this.instance.getNbCities()-2
 	}
 	
 	public Integer getPosition() {
 		return this.position;
 	}
 	
-	/*public void setPosition(int position) {
+	public void setPosition(int position) {
 		this.position = position;
-	}*/
-	
-	public ArrayList<Integer> getTrajet_fourmi(){
-		return this.getTrajet_fourmi();
 	}
 	
-	public Double getProba_chemin(int ville_i, int ville_j) {           //proba que la fourmi emprunte le chemin pour aller 
-																		//vers ville_j en étant à ville_i (si ville_j non visitée)
-		return this.Proba_trajet_entre_deux_villes[ville_i][ville_j];
+	public Solution getTrajet_fourmi(){
+		return this.Trajet_fourmi;
 	}
+	
+	
+	public Double depot_de_pheromone(double Q) throws Exception{
+		this.Trajet_fourmi.evaluate();
+		long objective_value_trajet=this.Trajet_fourmi.getObjectiveValue();
+		return Q/objective_value_trajet;
+		
+	}
+	
 }
