e55ae958e74686806c19daa9d6acf057d1ec5dc5
luckythomas <t.oustric@gmail.com>
2018-10-31 17:47:48 +0100

    fct get_ville_suivante qui retourne (en fct des proba) la prochaine vill

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -54,4 +54,33 @@ public class Fourmi {
 		
 	}
 	
+	public Double Proba_non_normalisee(Integer i, Integer j,Double alpha, Double beta, Double gamma, Double[][] Pheromone_trajet_entre_deux_villes) throws Exception{
+		return (gamma+Math.pow(Pheromone_trajet_entre_deux_villes[i][j],alpha)/Math.pow(this.instance.getDistances(i, j),beta));
+	}//renvoi la proba non normalisee (non divisée par la somme des proba des villes restantes)
+	
+	public Integer Get_ville_suivante(Double alpha, Double beta, Double gamma, Double[][] Pheromone_trajet_entre_deux_villes) throws Exception{
+		double somme_proba = 0;
+		for(int index_ville_suivante=0;index_ville_suivante<this.ville_non_visitees.size();index_ville_suivante++) {
+			somme_proba+=Proba_non_normalisee(this.position,this.ville_non_visitees.get(index_ville_suivante),alpha,beta,gamma,Pheromone_trajet_entre_deux_villes);
+		} // calcul de la somme des probas pour aller aux villes restantes ce qui nous servira à normaliser
+		double nb_hasard = Math.random(); //nombre au hasard entre 0 et 1
+		ArrayList<Double> Intervalle_proba = new ArrayList<Double>(); // contiendra les proba_normalisées (entre 0 et 1) et permettra de déterminer dans quel intervalle nb_hasard se trouvera
+		Intervalle_proba.add(0.0);
+		for(int i=0; i<this.ville_non_visitees.size();i++) {
+			Intervalle_proba.add(Proba_non_normalisee(this.position,this.ville_non_visitees.get(i),alpha,beta,gamma,Pheromone_trajet_entre_deux_villes)/somme_proba);
+		}
+		boolean ville_retenue = false; // boolean servant à ne plus rien faire dans la boucle for une fois l'intervalle trouvé
+		int ville_suivante=0; // initialisation de la variable servant à stocker la ville vers laquelle la fourmi ira
+		for(int i=0; i<Intervalle_proba.size();i++) {
+			if(ville_retenue == false && nb_hasard<Intervalle_proba.get(i)) {
+				ville_retenue = true; // dès que nb_hasard appartient à une "tranche"
+				ville_suivante = i; // on note le numéro de la ville à laquelle cette tranche corrsepond 
+									// cette ville sera celle vers laquelle la fourmi ira
+			}
+		}
+		return ville_suivante;
+		
+	}
+	
+	
 }
